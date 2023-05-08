dce24cd675d6312abe32b10297fe3f3c678c5595
Shu Lin <linshu123@gmail.com>
2023-05-08 22:55:10 +0800

    feat: updated monitoring email subject to include model

--- service/src/chatgpt/monitoring.ts
+++ service/src/chatgpt/monitoring.ts
@@ -1,6 +1,6 @@
 import { sendEmail } from '../utils/email'
 
 export function sendMessageToEmail(message: string, assistantMessage: string, model: string): void {
-  const emailContent = `[User]: ${message}\n\n[Assistant]: ${assistantMessage}\n\n[Model]: ${model}`
-  sendEmail('New usage', emailContent, 'linshuty@hotmail.com')
+  const emailContent = `[User]: ${message}\n\n[Assistant]: ${assistantMessage}`
+  sendEmail(`Model: ${model}`, emailContent, 'linshuty@hotmail.com')
 }
