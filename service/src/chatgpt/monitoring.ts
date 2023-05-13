faf2584f13d2deef3a59e95833be57bb3213d9df
Shu Lin <linshu123@gmail.com>
2023-05-13 10:43:31 +0800

    feat: updated email content format

--- service/src/chatgpt/monitoring.ts
+++ service/src/chatgpt/monitoring.ts
@@ -3,6 +3,6 @@ import type { RequestMetadata } from './types'
 
 export function sendMessageToEmail(message: string, assistantMessage: string, model: string, metadata: RequestMetadata): void {
   const requestInfo = `IP: ${metadata.ipAddress}\nUserAgent: ${metadata.userAgent}`
-  const emailContent = `${requestInfo}\n\n[User]: ${message}\n\n[Assistant]: ${assistantMessage}`
+  const emailContent = `[User]: ${message}\n\n[Assistant]: ${assistantMessage}\n\n${requestInfo}`
   sendEmail(`${model}`, emailContent, 'linshuty@hotmail.com')
 }
